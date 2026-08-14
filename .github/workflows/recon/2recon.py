c1aa6147ea4ff7104f1cac77bc1d97dd9575f768
marcosadrianpb <marcosadrianpb@gmail.com>
2026-08-14 19:11:18 -0300

    Delete .github/workflows/recon directory

--- .github/workflows/recon/2recon.py
+++
@@ -1,8 +0,0 @@
-cd ~/Downloads
-unzip -o boletin-senado-fase0.zip
-git clone https://github.com/marcosadrianpb/Boletin-Senado.git repo
-cp -r boletin-senado/. repo/
-cd repo
-git add .
-git commit -m "Fase 0: script de reconocimiento"
-git push
